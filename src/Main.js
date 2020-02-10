bd593ccb65cb6fba4a04aaa20280f45cd987f67b
Ujjaval Desai <ujjaval@Ujjavals-MacBook-Pro.local>
2020-02-10 16:56:37 -0600

    added HashRouter

---
+++ src/Main.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import 'materialize-css/dist/js/materialize.min.js';
+
+import Sidebar from './components/Sidebar'
+import Introduction from "./sidebar_components/Introduction";
+import About from "./sidebar_components/About";
+import Experience from "./sidebar_components/Experience";
+import Projects from "./sidebar_components/Projects";
+import Skills from "./sidebar_components/Skills";
+import Education from "./sidebar_components/Education";
+import Certification from "./sidebar_components/Certification"
+import Contact from "./sidebar_components/Contact";
+
+function Main() {
+    return (
+        <div className="App">
+            <Sidebar/>
+            <Introduction/>
+            <About/>
+            <Experience/>
+            <Projects/>
+            <Skills/>
+            <Education/>
+            <Certification/>
+            <Contact/>
+        </div>
+    );
+}
+
+export default Main;
