c1207e5d3113dc116ea8bb07c3ef063934f74964
Ujjaval Desai <ujjaval@Ujjavals-MacBook-Pro.local>
2020-02-10 17:24:09 -0600

    added hashrouter

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
 import React from 'react';
 import './App.css';
-import 'materialize-css/dist/js/materialize.min.js';
-
-import Sidebar from './components/Sidebar'
-import Introduction from "./sidebar_components/Introduction";
-import About from "./sidebar_components/About";
-import Experience from "./sidebar_components/Experience";
-import Projects from "./sidebar_components/Projects";
-import Skills from "./sidebar_components/Skills";
-import Education from "./sidebar_components/Education";
-import Certification from "./sidebar_components/Certification"
-import Contact from "./sidebar_components/Contact";
+import Main from "./Main";
+import {HashRouter, Route, Link, Switch} from "react-router-dom";
 
 function App() {
     return (
-        <div className="App">
-            <Sidebar/>
-            <Introduction/>
-            <About/>
-            <Experience/>
-            <Projects/>
-            <Skills/>
-            <Education/>
-            <Certification/>
-            <Contact/>
-        </div>
+        <HashRouter>
+        <Switch>
+            <Route path="/" component={Main}/>
+        </Switch>
+        </HashRouter>
     );
 }
 
