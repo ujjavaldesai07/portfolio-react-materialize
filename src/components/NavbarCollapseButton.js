52a0e17eafb263c3b5f4538b5ab9946f5d3f4b97
Ujjaval Desai <ujjaval@Ujjavals-MacBook-Pro.local>
2020-02-12 19:53:32 -0600

    fix the default scrolling to top after press toggle menu

--- src/components/NavbarCollapseButton.js
+++ src/components/NavbarCollapseButton.js
@@ -16,9 +16,10 @@ class NavbarCollapseButton extends React.Component {
         return (
             <div className="navbar-fixed">
                 <nav className="hide-on-large-only z-depth-1">
-                    <a href="#" data-target="slide-out" className="sidenav-trigger"
+
+                    <div data-target="slide-out" className="sidenav-trigger"
                     ><i className="material-icons md-dark md-36 teal-text">menu</i
-                    ></a>
+                    ></div>
 
                     <div className="name-title">
                         <a href="#Introduction" className="teal-text">Ujjaval Desai
