8a411b3cdab753dbad19ce7109ff14b79a51bbc9
Ujjaval Desai <ujjavaldesai5891@gmail.com>
2020-03-03 13:16:06 -0600

    Update Resume.js

--- src/sidebar_components/Resume.js
+++ src/sidebar_components/Resume.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'materialize-css/dist/js/materialize.min.js';
 import resumeImage from '../images/navbar/resume.png';
 
-const RESUME_URL = 'https://drive.google.com/file/d/1cDnjV6bJvQr4jTuUZHS6MxVrW7J47zN3/view?usp=sharing';
+const RESUME_URL = 'https://drive.google.com/file/d/1S1iGFDgWKJ8F0ZVQuCZGW4Yp-TVEmp-U/view?usp=sharing';
 
 class Resume extends React.Component {
 
