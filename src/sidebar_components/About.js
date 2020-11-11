14293d67f2a521c35a9fd20a30462d756f70dfc6
ujjaval <ujjavaldesai5891@gmail.com>
2020-11-11 13:30:38 -0800

    Reduced the font-size of About page text.

--- src/sidebar_components/About.js
+++ src/sidebar_components/About.js
@@ -11,26 +11,38 @@ class About extends React.Component {
                     <div className="pb-3">
                         <blockquote className="brown-text">
                             <h2>
-                                Goal-oriented software developer with a passion to build applications that make a difference.
+                                Goal-oriented software developer with a passion to build applications that make a
+                                difference.
                             </h2>
                         </blockquote>
                     </div>
-                    <div>
-                        <p>
-                            I've 4+ years of experience as a Senior Software Engineer working in Deep Packet Inspection
-                            technologies of the networking domain. Experienced in software development with C/C++
-                            in Unix environment. Equipped with diverse and promising skill-set. Good interpersonal
-                            and communication skills. Built personal projects using trending languages like Java and
-                            JavaScript using frameworks, databases, and build tools like Spring Boot, React,
-                            Material UI, MySQL, MongoDB, Redis, Webpack, Docker, etc. Currently, I am pursuing my
-                            Master's degree in Computer Science and I will be graduating in December 2020.
-                        </p>
-                        <p>
-                            As a Software Developer, I am always obsessed with minor details, learning new technologies,
-                            and building innovative features in an application. At present, I am looking for an
-                            opportunity to contribute my professional knowledge at the right place and enhance my
-                            skills for further growth and betterment.
-                        </p>
+                    <div className="row mt-3">
+                        <div className="col s12 m5">
+                            <div className="card-panel">
+                                <p>
+                                    I've 4+ years of experience as a Senior Software Engineer working in Deep Packet
+                                    Inspection
+                                    technologies of the networking domain. Experienced in software development with
+                                    C/C++
+                                    in Unix environment. Equipped with diverse and promising skill-set. Good
+                                    interpersonal
+                                    and communication skills. Built personal projects using trending languages like Java
+                                    and
+                                    JavaScript using frameworks, databases, and build tools like Spring Boot, React,
+                                    Material UI, MySQL, MongoDB, Redis, Webpack, Docker, etc. Currently, I am pursuing
+                                    my
+                                    Master's degree in Computer Science and I will be graduating in December 2020.
+                                </p>
+                                <p>
+                                    As a Software Developer, I am always obsessed with minor details, learning new
+                                    technologies,
+                                    and building innovative features in an application. At present, I am looking for an
+                                    opportunity to contribute my professional knowledge at the right place and enhance
+                                    my
+                                    skills for further growth and betterment.
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </>
