c13925e8621bdd0c7753dcf8915dd021b27af1cb
ujjaval <ujjavaldesai5891@gmail.com>
2020-12-23 10:08:20 -0800

    updated about page.

--- src/sidebar_components/About.js
+++ src/sidebar_components/About.js
@@ -23,21 +23,21 @@ class About extends React.Component {
                             <div className="card-panel">
                                 <p>
                                     Hello! I am a recent graduate (December 2020) with a Master’s degree in Computer
-                                    Science with strong academic performance (3.96 GPA). In past, I have gained 4+ years of
-                                    experience in software development with C++ and Deep Packet Inspection
-                                    technologies of the Networking domain. I had worked as a Senior Software Engineer
-                                    at Sandvine in 2014 and I had a great experience working in such a professional
+                                    Science with strong academic performance (3.96 GPA). In past, I have gained 4+ years
+                                    of experience in software development with C++ and Deep Packet Inspection
+                                    technologies of the Networking domain. I had worked as a Senior Software Engineer at
+                                    Sandvine in 2014 and I had a great experience working in such a professional
                                     environment.
                                 </p>
                                 <p>
                                     I am passionate about coding and building real-world applications which
                                     gives me a better understanding in terms of performance, scalability,
                                     user experience, etc. I have built personal projects such as a shopping website,
-                                    chat application, self-assessment tool, etc. These applications are built using
-                                    Java, JavaScript, and CSS frameworks like Spring Boot, Node.js, React, Material UI, etc.
-                                    I have also used databases and tools in my project such as MySQL, MongoDB, Redis,
-                                    Docker, Webpack, etc. All of my personal projects are deployed on Heroku, AWS and
-                                    the source code is available on GitHub.
+                                    chat application, self-assessment tool, etc. These applications are built in Java
+                                    and JavaScript using various frameworks like Spring Boot, Express, React, Material
+                                    UI, etc. I have also used databases and tools in my project such as MySQL, MongoDB,
+                                    Redis, Docker, Webpack, etc. All of my personal projects are deployed on Heroku, AWS
+                                    and the source code is available on GitHub.
                                 </p>
                                 <p>
                                     As a Software Engineer, I am always obsessed with minor details, learning new
